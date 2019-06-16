258b07d32ed8d59764d10456729ea26e58d02f78
Kevin <mybaseball52@gmail.com>
2019-06-16 23:04:35 +0800

    add ITTO and tooltips

--- data/pmp.js
+++ data/pmp.js
@@ -5,7 +5,23 @@ let sub_process = [
         knowledge_area: "Integration",
         sub_process: "Develop Project Charter",
         zh_sub_process: "發展專案章程",
-        sentence: "奉天承運 - 發起人詔曰，啟動專案授權PM"
+        sentence: "奉天承運 - 發起人詔曰，啟動專案授權PM",
+        input: {
+            "商業文件": "商業企劃案",
+            "協議": "契約",
+            "企業環境因素": "待補充", 
+            "組織過程資產" : "待補充"
+        },
+        techandtool: {
+            "專家判斷":"待補充", 
+            "資料蒐集": "待補充",
+            "人際關係與團隊技巧": "待補充",
+            "會議": "待補充"
+        },
+        output: {
+            "專案章程": "待補充", 
+            "假設記錄": "待補充"
+        }
     },
     {
         id: 1,
@@ -13,7 +29,10 @@ let sub_process = [
         knowledge_area: "Stakeholder",
         sub_process: "Identify Stakeholders",
         zh_sub_process: "辨識利害關係人",
-        sentence: "辨識關係人，分析影響度，記下關切點，專案成功失誤少"
+        sentence: "辨識關係人，分析影響度，記下關切點，專案成功失誤少",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 2,
@@ -21,7 +40,10 @@ let sub_process = [
         knowledge_area: "Integration",
         sub_process: "Develop Project Management Plan",
         zh_sub_process: "發展專案管理計畫書",
-        sentence: "專案計畫書涵蓋 10+2 的計畫與三大基準，指導大家如何完成專案達成目標"
+        sentence: "專案計畫書涵蓋 10+2 的計畫與三大基準，指導大家如何完成專案達成目標",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 3,
@@ -29,7 +51,10 @@ let sub_process = [
         knowledge_area: "Scope",
         sub_process: "Plan Scope Management",
         zh_sub_process: "規劃範疇管理",
-        sentence: "如何蒐集需求，如何管理專案範圍？教戰手冊完成先"
+        sentence: "如何蒐集需求，如何管理專案範圍？教戰手冊完成先",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 4,
@@ -37,7 +62,10 @@ let sub_process = [
         knowledge_area: "Scope",
         sub_process: "Collect Requirements",
         zh_sub_process: "蒐集需求",
-        sentence: "民之所欲，常在我心"
+        sentence: "民之所欲，常在我心",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 5,
@@ -45,7 +73,10 @@ let sub_process = [
         knowledge_area: "Planning",
         sub_process: "Define Scope",
         zh_sub_process: "定義範疇",
-        sentence: "定義範疇說明書，專案範圍有依據，允收條件列清楚"
+        sentence: "定義範疇說明書，專案範圍有依據，允收條件列清楚",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 6,
@@ -53,7 +84,10 @@ let sub_process = [
         knowledge_area: "Planning",
         sub_process: "Create WBS",
         zh_sub_process: "建立工作分解結構",
-        sentence: "大卸八塊，化整為零，並將每一塊加以定義，發展成範疇基準"
+        sentence: "大卸八塊，化整為零，並將每一塊加以定義，發展成範疇基準",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 7,
@@ -61,7 +95,10 @@ let sub_process = [
         knowledge_area: "Schedule",
         sub_process: "Plan Schedule Management",
         zh_sub_process: "發展專案管理計畫書",
-        sentence: "時程計畫要做好，專案進度能確保"
+        sentence: "時程計畫要做好，專案進度能確保",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 8,
@@ -69,7 +106,10 @@ let sub_process = [
         knowledge_area: "Schedule",
         sub_process: "Define Activity",
         zh_sub_process: "定義活動",
-        sentence: "定義要做什麼活動來完成可交付成果"
+        sentence: "定義要做什麼活動來完成可交付成果",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 9,
@@ -77,7 +117,10 @@ let sub_process = [
         knowledge_area: "Schedule",
         sub_process: "Sequence Activity",
         zh_sub_process: "排序活動",
-        sentence: "誰先誰後說分明，看清彼此相依關係，畫成網路才清楚"
+        sentence: "誰先誰後說分明，看清彼此相依關係，畫成網路才清楚",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 10,
@@ -85,7 +128,10 @@ let sub_process = [
         knowledge_area: "Schedule",
         sub_process: "Estimate Activity Duration",
         zh_sub_process: "估算活動期程",
-        sentence: "以取得的資源，估算活動工期"
+        sentence: "以取得的資源，估算活動工期",
+        input: {},
+        techandtool: {},
+        output: {}
     },
     {
         id: 11,
@@ -93,7 +139,43 @@ let sub_process = [
         knowledge_area: "Schedule",
         sub_process: "Develop Schedule",
         zh_sub_process: "發展時程",
-        sentence: "抓要徑，壓時程，建立進度基準"
+        sentence: "抓要徑，壓時程，建立進度基準",
+        input: {},
+        techandtool: {},
+        output: {}
+    },
+    {
+        id: 12,
+        process: "Planning",
+        knowledge_area: "Cost",
+        sub_process: "Plan Cost Management",
+        zh_sub_process: "規劃成本管理",
+        sentence: "成本計畫很重要，資金運用及管理有方向",
+        input: {},
+        techandtool: {},
+        output: {}
+    },
+    {
+        id: 13,
+        process: "Planning",
+        knowledge_area: "Cost",
+        sub_process: "Estimate Costs",
+        zh_sub_process: "估算成本",
+        sentence: "估算專案活動所需人、機、材各項成本，儲備金也不可少，成本估算才完整",
+        input: {},
+        techandtool: {},
+        output: {}
+    },
+    {
+        id: 14,
+        process: "Planning",
+        knowledge_area: "Cost",
+        sub_process: "Determine Budget",
+        zh_sub_process: "決定預算",
+        sentence: "先有經費與應變金做基準，再加管理儲備金以備不時之需，敲定總資金！",
+        input: {},
+        techandtool: {},
+        output: {}
     },
 ]
 
