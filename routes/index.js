f55255433b1d4f060990ca606bc6090c26fdc217
Kevin <mybaseball52@gmail.com>
2019-09-03 23:55:21 +0800

    change UI to angular

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,11 @@ let router = express.Router();
 let data = require('../data/pmp');
 
 router.get('/', function (req, res, next) {
-  data = data.sort( () => Math.random() - 0.5)
-  console.log(data[0])
   res.render("index",{knowledges: [data[0]]});
 });
 
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
+router.get('/data', function (req, res, next) {
+  res.json(data);
+});
 
 module.exports = router;
