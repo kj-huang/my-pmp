142dd1ded42228a265d54bccebf3edfe19979a59
Kevin <mybaseball52@gmail.com>
2019-09-03 23:30:17 +0800

    update UI design and add ITTO data

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ let router = express.Router();
 let data = require('../data/pmp');
 
 router.get('/', function (req, res, next) {
-  res.render("index",{knowledges: data});
+  data = data.sort( () => Math.random() - 0.5)
+  console.log(data[0])
+  res.render("index",{knowledges: [data[0]]});
 });
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
 module.exports = router;
