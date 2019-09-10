119d9c91958bdba8b469f70c9a34fa611d704346
Kevin <mybaseball52@gmail.com>
2019-09-10 09:20:22 +0800

    stash

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,30 @@
 let express = require('express');
 let router = express.Router();
 let data = require('../data/pmp');
+let freq = require('../data/freq');
 
 router.get('/', function (req, res, next) {
   res.render("index",{knowledges: [data[0]]});
 });
 
+router.get('/home', function (req, res, next) {
+  res.render("home");
+});
+
+router.get('/abbreviation', function (req, res, next) {
+  res.render("abbreviation");
+});
+
+router.get('/abbreviation', function (req, res, next) {
+  res.render("abbreviation");
+});
+
 router.get('/data', function (req, res, next) {
-  res.json(data);
+  res.render('freq', freq);
+});
+
+router.get('/freq', function (req, res, next) {
+  res.json(freq);
 });
 
 module.exports = router;
