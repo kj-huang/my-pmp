94068e3d3d8dad5657c25fe59a27ae213e2be0b5
mybaseball52 <mybaseball52@gmail.com>
2019-10-27 22:37:14 +0800

    add all documents selections

---
+++ data/answer.js
@@ -0,0 +1,538 @@
+let answer = [{
+  "id": 1,
+  "question": "What is the output of 發展專案章程",
+  "selections": [{
+    "name": "專案章程",
+    "answer": true
+  }, {
+    "name": "假設紀錄",
+    "answer": true
+  }]
+}, {
+  "id": 2,
+  "question": "What is the output of 發展專案管理計畫書",
+  "selections": [{
+    "name": "專案管理計畫書",
+    "answer": true
+  }]
+}, {
+  "id": 3,
+  "question": "What is the output of 指導及管理專案工作",
+  "selections": [{
+    "name": "可交付成果",
+    "answer": true
+  }, {
+    "name": "工作績效資料",
+    "answer": true
+  }, {
+    "name": "議題紀錄",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 4,
+  "question": "What is the output of 管理專案知識",
+  "selections": [{
+    "name": "經驗學習登錄表",
+    "answer": true
+  }]
+}, {
+  "id": 5,
+  "question": "What is the output of 監視與管制專案工作",
+  "selections": [{
+    "name": "工作績效報告",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 6,
+  "question": "What is the output of 進行整合變更控制",
+  "selections": [{
+    "name": "獲准的變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 7,
+  "question": "What is the output of 結束專案或階段",
+  "selections": [{
+    "name": "最終產品、服務或成果轉移",
+    "answer": true
+  }, {
+    "name": "最終報告",
+    "answer": true
+  }]
+}, {
+  "id": 9,
+  "question": "What is the output of 規劃範疇管理",
+  "selections": [{
+    "name": "範疇管理計畫書",
+    "answer": true
+  }, {
+    "name": "需求管理計畫書",
+    "answer": true
+  }]
+}, {
+  "id": 10,
+  "question": "What is the output of 蒐集需求",
+  "selections": [{
+    "name": "需求相關文件",
+    "answer": true
+  }, {
+    "name": "需求追溯矩陣",
+    "answer": true
+  }]
+}, {
+  "id": 11,
+  "question": "What is the output of 定義範疇",
+  "selections": [{
+    "name": "專案範疇說明書",
+    "answer": true
+  }]
+}, {
+  "id": 12,
+  "question": "What is the output of 建立工作分解結構",
+  "selections": [{
+    "name": "範疇基準",
+    "answer": true
+  }]
+}, {
+  "id": 13,
+  "question": "What is the output of 確認範疇",
+  "selections": [{
+    "name": "已接受的可交付成果",
+    "answer": true
+  }, {
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 14,
+  "question": "What is the output of 管制範疇",
+  "selections": [{
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 16,
+  "question": "What is the output of 規劃時程管理",
+  "selections": [{
+    "name": "時程管理計畫書",
+    "answer": true
+  }]
+}, {
+  "id": 17,
+  "question": "What is the output of 定義活動",
+  "selections": [{
+    "name": "活動清單",
+    "answer": true
+  }, {
+    "name": "活動屬性",
+    "answer": true
+  }, {
+    "name": "里程碑清單",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 18,
+  "question": "What is the output of 排序活動",
+  "selections": [{
+    "name": "專案時程網路圖",
+    "answer": true
+  }]
+}, {
+  "id": 19,
+  "question": "What is the output of 估算活動期程(P)",
+  "selections": [{
+    "name": "期程估計",
+    "answer": true
+  }, {
+    "name": "估算基礎",
+    "answer": true
+  }]
+}, {
+  "id": 20,
+  "question": "What is the output of 發展時程(P)",
+  "selections": [{
+    "name": "時程基準",
+    "answer": true
+  }, {
+    "name": "專案時程",
+    "answer": true
+  }, {
+    "name": "時程資料",
+    "answer": true
+  }, {
+    "name": "專案行事曆",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 21,
+  "question": "What is the output of 管制時程(M)",
+  "selections": [{
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "時程預測值",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 23,
+  "question": "What is the output of 規劃成本管理",
+  "selections": [{
+    "name": "成本管理計畫書",
+    "answer": true
+  }]
+}, {
+  "id": 24,
+  "question": "What is the output of 估算成本",
+  "selections": [{
+    "name": "成本估算",
+    "answer": true
+  }, {
+    "name": "估算基礎",
+    "answer": true
+  }]
+}, {
+  "id": 25,
+  "question": "What is the output of 決定預算",
+  "selections": [{
+    "name": "成本基準",
+    "answer": true
+  }, {
+    "name": "專案資金需求",
+    "answer": true
+  }]
+}, {
+  "id": 26,
+  "question": "What is the output of 管制成本",
+  "selections": [{
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "成本預測值",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 28,
+  "question": "What is the output of 規劃品質管理",
+  "selections": [{
+    "name": "品質管理計畫書",
+    "answer": true
+  }, {
+    "name": "品質指標",
+    "answer": true
+  }]
+}, {
+  "id": 29,
+  "question": "What is the output of 管理品質",
+  "selections": [{
+    "name": "品質報告",
+    "answer": true
+  }, {
+    "name": "測試與評估文件",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 30,
+  "question": "What is the output of 管制品質",
+  "selections": [{
+    "name": "品質管制衡量值",
+    "answer": true
+  }, {
+    "name": "已驗收的可交付成果",
+    "answer": true
+  }, {
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 32,
+  "question": "What is the output of 規劃資源管理",
+  "selections": [{
+    "name": "資源管理計畫書",
+    "answer": true
+  }, {
+    "name": "團隊章程",
+    "answer": true
+  }]
+}, {
+  "id": 33,
+  "question": "What is the output of 估算活動資源",
+  "selections": [{
+    "name": "資源需求",
+    "answer": true
+  }, {
+    "name": "估算基礎",
+    "answer": true
+  }, {
+    "name": "資源分解結構",
+    "answer": true
+  }]
+}, {
+  "id": 34,
+  "question": "What is the output of 獲得資源",
+  "selections": [{
+    "name": "實體資源分派",
+    "answer": true
+  }, {
+    "name": "專案團隊分派",
+    "answer": true
+  }, {
+    "name": "資源行事曆",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }, {
+    "name": "企業環境因素更新",
+    "answer": true
+  }]
+}, {
+  "id": 35,
+  "question": "What is the output of 發展專案團隊",
+  "selections": [{
+    "name": "團隊績效評量",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }, {
+    "name": "企業環境因素更新",
+    "answer": true
+  }]
+}, {
+  "id": 36,
+  "question": "What is the output of 管理專案團隊(E)",
+  "selections": [{
+    "name": "變更申請",
+    "answer": true
+  }, {
+    "name": "專案文件更新(議題紀錄更新)",
+    "answer": true
+  }, {
+    "name": "企業環境因素更新",
+    "answer": true
+  }]
+}, {
+  "id": 37,
+  "question": "What is the output of 管制資源(M)",
+  "selections": [{
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 39,
+  "question": "What is the output of 規劃溝通管理",
+  "selections": [{
+    "name": "溝通管理計畫書",
+    "answer": true
+  }]
+}, {
+  "id": 40,
+  "question": "What is the output of 管理溝通",
+  "selections": [{
+    "name": "專案溝通",
+    "answer": true
+  }]
+}, {
+  "id": 41,
+  "question": "What is the output of 監視溝通",
+  "selections": [{
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 43,
+  "question": "What is the output of 規劃風險管理",
+  "selections": [{
+    "name": "風險管理計畫書",
+    "answer": true
+  }]
+}, {
+  "id": 44,
+  "question": "What is the output of 辨識風險",
+  "selections": [{
+    "name": "風險登錄表",
+    "answer": true
+  }, {
+    "name": "風險報告",
+    "answer": true
+  }]
+}, {
+  "id": 45,
+  "question": "What is the output of 進行定性風險分析",
+  "selections": [{
+    "name": "專案文件更新(風險登錄表更新)",
+    "answer": true
+  }]
+}, {
+  "id": 46,
+  "question": "What is the output of 進行定量風險分析",
+  "selections": [{
+    "name": "專案文件更新(風險登錄表更新)",
+    "answer": true
+  }]
+}, {
+  "id": 47,
+  "question": "What is the output of 規劃風險回應",
+  "selections": [{
+    "name": "變更申請",
+    "answer": true
+  }, {
+    "name": "專案文件更新(風險登錄表更新)",
+    "answer": true
+  }]
+}, {
+  "id": 48,
+  "question": "What is the output of 實施風險回應",
+  "selections": [{
+    "name": "變更申請",
+    "answer": true
+  }, {
+    "name": "專案文件更新(風險登錄表更新)",
+    "answer": true
+  }]
+}, {
+  "id": 49,
+  "question": "What is the output of 監視風險",
+  "selections": [{
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }, {
+    "name": "專案文件更新(風險登錄表更新)",
+    "answer": true
+  }]
+}, {
+  "id": 51,
+  "question": "What is the output of 規劃採購管理",
+  "selections": [{
+    "name": "採購管理計畫書",
+    "answer": true
+  }, {
+    "name": "採購策略",
+    "answer": true
+  }, {
+    "name": "投(招)標文件",
+    "answer": true
+  }, {
+    "name": "採購工作說明書",
+    "answer": true
+  }, {
+    "name": "商源評選準則",
+    "answer": true
+  }, {
+    "name": "自製或外購決策",
+    "answer": true
+  }, {
+    "name": "獨立成本估算",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 52,
+  "question": "What is the output of 執行採購",
+  "selections": [{
+    "name": "選定的賣方",
+    "answer": true
+  }, {
+    "name": "協議",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 53,
+  "question": "What is the output of 管制採購",
+  "selections": [{
+    "name": "結束採購",
+    "answer": true
+  }, {
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "採購相關文件更新",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 55,
+  "question": "What is the output of 辨識利害關係人",
+  "selections": [{
+    "name": "利害關係人登錄表",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}, {
+  "id": 56,
+  "question": "What is the output of 規劃利害關係人參與",
+  "selections": [{
+    "name": "利害關係人管理計畫書",
+    "answer": true
+  }]
+}, {
+  "id": 57,
+  "question": "What is the output of 管理利害關係人參與",
+  "selections": [{
+    "name": "變更申請",
+    "answer": true
+  }, {
+    "name": "專案文件更新(議題紀錄更新)",
+    "answer": true
+  }]
+}, {
+  "id": 58,
+  "question": "What is the output of 監視利害關係人參與",
+  "selections": [{
+    "name": "工作績效資訊",
+    "answer": true
+  }, {
+    "name": "變更申請",
+    "answer": true
+  }]
+}];
+
+module.exports = answer;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let router = express.Router();
 let data = require('../data/pmp');
+let answer = require('../data/answer');
+let documents = require('../data/documents');
 
 router.get('/', function (req, res, next) {
   res.render("index",{knowledges: [data[0]]});
@@ -11,16 +13,26 @@ router.get('/data', function (req, res, next) {
 });
 
 router.get('/question', function (req, res, next) {
-  let question = [
-    {"id": 1, "question": "What is the output of 發展專案章程","selections": [
-      {"name": "專案章程", "answer": true},
-      {"name": "假設紀錄", "answer": true},
-      {"name": "專案管理計劃書", "answer": false},
-      {"name": "商業企劃案", "answer": false},
-      {"name": "變更申請", "answer": false}
-      ]}
-  ]
+  let q = answer.sort( () => Math.random() - 0.5)[0];
+  
+  //shuffle
+  documents.sort( () => Math.random() - 0.5);
 
+  let count = 0;
+  documents.forEach((d) => {
+    if(count < 7 && q.selections.length < 7 && q.selections.name !== d){
+      let o = {};
+      o.name = d;
+      o.answer = false;
+      q.selections.push(o)
+    }
+  })
+
+  q.selections.sort( () => Math.random() - 0.5);
+
+
+  
+  let question = [q]
   
   res.render("five_question", {question: question});
 });
