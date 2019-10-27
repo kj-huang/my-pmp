7375fa08ef94482bb4659cf9421f164c721a8027
mybaseball52 <mybaseball52@gmail.com>
2019-10-27 17:12:39 +0800

    add itto test function prototype

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,20 @@ router.get('/data', function (req, res, next) {
   res.json(data);
 });
 
+router.get('/question', function (req, res, next) {
+  let question = [
+    {"id": 1, "question": "What is the output of 發展專案章程","selections": [
+      {"name": "專案章程", "answer": true},
+      {"name": "假設紀錄", "answer": true},
+      {"name": "專案管理計劃書", "answer": false},
+      {"name": "商業企劃案", "answer": false},
+      {"name": "變更申請", "answer": false}
+      ]}
+  ]
+
+  
+  res.render("five_question", {question: question});
+});
+
+
 module.exports = router;
